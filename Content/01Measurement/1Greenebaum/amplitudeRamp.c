803b233f4e29f8559827dcd5577303929171b9cf
keng <keng>
2003-03-12 10:49:05 +0000

    *** empty log message ***

--- Content/01Measurement/1Greenebaum/amplitudeRamp.c
+++ Content/01Measurement/1Greenebaum/amplitudeRamp.c
@@ -1,3 +1,8 @@
+// amplitude ramp
+//
+// play the refference tone and print output
+// then play -3 dB, -6dB, -10dB, etc.
+
 #include <math.h>     // sin()
 #include "pablio.h"
 
@@ -5,20 +10,28 @@
 #define FREQUENCY      440.0
 #define NUM_FRAMES    1000
 
+#define INCTIME		0.5
+#define RATIO		0.9
+
 float buffer[NUM_FRAMES];
 
 int main(void)
 {
    PABLIO_Stream  *outStream;
-   double theta, delta = FREQUENCY * 2.0 * 3.1415 / (double)SAMPLE_RATE;
+   double theta=0.0, delta = FREQUENCY * 2.0 * 3.1415 / (double)SAMPLE_RATE;
+   int incFrames = INCTIME * SAMPLE_RATE;
    int x; 
+   int count = 0;
+   double mult = 1.0;
 
    OpenAudioStream(&outStream, SAMPLE_RATE, paFloat32, PABLIO_WRITE|PABLIO_MONO);
 
    while(1) {  // synthesize and output samples forever
       for(x= 0; x<NUM_FRAMES; x++) { // synthesize a buffer full of samples
-           buffer[x++] = sin(theta); // ugly, I know...
+           buffer[x] = mult * sin(theta); // ugly, I know...
            theta+= delta;
+	   if(!(count++ % incFrames))
+	      mult*= RATIO;
       }
 
       // blocking write provides flow control
