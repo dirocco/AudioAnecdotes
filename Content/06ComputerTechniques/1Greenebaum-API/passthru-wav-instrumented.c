45dff05f922f52268f15d69423c8cde6cd5a75ea
keng <keng>
2003-03-19 23:40:44 +0000

    *** empty log message ***

--- Content/06ComputerTechniques/1Greenebaum-API/passthru-wav-instrumented.c
+++ Content/06ComputerTechniques/1Greenebaum-API/passthru-wav-instrumented.c
@@ -1,4 +1,4 @@
-// Microsoft libmm waveio based passthru example
+// Microsoft libmm waveio based passthru
 //
 
 #include <stdio.h>
@@ -7,6 +7,9 @@
 
 int main()
 {
+   // MMRESULT result;
+   int inIndex, outIndex;
+
    HWAVEIN   inStream;
    HWAVEOUT outStream;
    WAVEFORMATEX waveFormat;
@@ -38,7 +41,16 @@ int main()
 	 (void *)malloc(NUM_FRAMES * waveFormat.nBlockAlign);
 
       buffer[index].dwFlags        = 0;
+/*
+      if(index<2)
+	 waveInPrepareHeader(  inStream, &buffer[index], sizeof(WAVEHDR));
+      else
+	 waveOutPrepareHeader(outStream, &buffer[index], sizeof(WAVEHDR));
+*/
       waveInPrepareHeader(  inStream, &buffer[index], sizeof(WAVEHDR));
+
+
+      //buffer[index].dwFlags       |= WHDR_DONE;
    }
 
    ResetEvent(event);
@@ -46,22 +58,41 @@ int main()
       waveInAddBuffer(inStream, &buffer[index], sizeof(WAVEHDR));
    waveInStart(inStream);
     
-   while(!( buffer[1].dwFlags & WHDR_DONE)); // poll(!) for 2 full input buffers
+   // buffer[2].dwFlags |= WHDR_DONE;
+   // buffer[3].dwFlags |= WHDR_DONE;
+
+   // collect 2 full inputs before continuing?
+   unsigned long count = 0;
+   while(!( buffer[0].dwFlags & WHDR_DONE))
+      count++;
+   printf("I[0] %f\n", count/100000.0);
+
+   while(!( buffer[1].dwFlags & WHDR_DONE))
+      count++;
+   printf("I[1] %f\n", count/100000.0);
+
+   inIndex = 2; // the next (filling) inBuffer to watch
 
    // move the two full buffers to output
    waveOutWrite(outStream, &buffer[0], sizeof(WAVEHDR));
    waveOutWrite(outStream, &buffer[1], sizeof(WAVEHDR));
+   outIndex = 0; // the next (draining) outbuffer to watch
 
-   int inIndex = 2, outIndex = 0; // the next input and output to watch
-   while(1) {  // poll for completed input and output buffers
-      if(buffer[inIndex].dwFlags & WHDR_DONE) { // input buffer complete?
-	 waveInAddBuffer(  inStream, &buffer[inIndex],  sizeof(WAVEHDR));
-	 inIndex = (inIndex+1)%4;   // next buffer to watch for full
+
+   while(1) {
+      if(buffer[inIndex].dwFlags & WHDR_DONE) {
+	 printf("I[%d] %f\n", inIndex, count/100000.0);
+	 waveInAddBuffer(  inStream, &buffer[inIndex], sizeof(WAVEHDR));
+	 inIndex = (inIndex+1)%4;   // next buffer
       }
 
-      if(buffer[outIndex].dwFlags & WHDR_DONE) { // output buffer complete?
-	 waveOutWrite(    outStream, &buffer[outIndex], sizeof(WAVEHDR));
-	 outIndex = (outIndex+1)%4;   // next buffer to watch for empty
+      if(buffer[outIndex].dwFlags & WHDR_DONE) {
+	 printf("O[%d] %f\n", outIndex, count/100000.0);
+	 waveOutWrite(outStream, &buffer[outIndex], sizeof(WAVEHDR));
+	 outIndex = (outIndex+1)%4;   // next buffer
       }
+      
+      count++;
    }
+
 }
