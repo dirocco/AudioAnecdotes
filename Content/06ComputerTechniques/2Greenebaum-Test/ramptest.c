3b243d7df71a4216600249aa6102a2fd9e41585e
keng <keng>
2003-03-23 02:55:58 +0000

    *** empty log message ***

--- Content/06ComputerTechniques/2Greenebaum-Test/ramptest.c
+++ Content/06ComputerTechniques/2Greenebaum-Test/ramptest.c
@@ -1,3 +1,37 @@
 main()
 {
+typedef unsigned short USHORT;
+
+USHORT value = 0, lastval = 0;
+char state = 0;
+USHORT count = 0;
+
+   while(1) {
+      lastval = value;
+      scanf("%d", &value);
+      count++;
+
+      switch(state) {
+	 case 0:  // wait for signal
+	    if(value) {
+	       printf("found signal at sample %d\n", count-2);
+	       state = 1;
+	    }
+	 break;
+
+	 case 1: 
+	    if(value != ((USHORT)((USHORT)lastval+(USHORT)1)))
+	       printf("%d %d %d ", value, lastval, (USHORT)((USHORT)lastval+(USHORT)1)),
+	       printf("we have a problem at %d\n", count -1);
+	 break;
+		 
+	 default:
+	    printf("I fell and I can't get up\n");
+      }
+
+
+   }
+
+
+
 }
