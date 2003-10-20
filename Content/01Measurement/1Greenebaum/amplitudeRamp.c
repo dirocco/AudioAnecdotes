d46f9911332d23f64fa37076dfa69e77f8baaac2
keng <keng>
2003-10-20 11:01:03 +0000

    added parameter and image to document

--- Content/01Measurement/1Greenebaum/amplitudeRamp.c
+++ Content/01Measurement/1Greenebaum/amplitudeRamp.c
@@ -1,13 +1,14 @@
 // amplitude ramp
 //
-// play the refference tone and print output
-// then play -3 dB, -6dB, -10dB, etc.
+// play a series of tones each at a lower amplitude
 
 #include <math.h>     // sin()
+#include <stdio.h>
+#include <stdlib.h>
 #include "pablio.h"
 
 #define SAMPLE_RATE  44100
-#define FREQUENCY      440.0
+#define FREQUENCY      441.0
 #define NUM_FRAMES    1000
 
 #define INCTIME		0.5
@@ -15,7 +16,7 @@
 
 float buffer[NUM_FRAMES];
 
-int main(void)
+int main(int argc, char *argv[])
 {
    PABLIO_Stream  *outStream;
    double theta=0.0, delta = FREQUENCY * 2.0 * 3.1415 / (double)SAMPLE_RATE;
@@ -23,6 +24,11 @@ int main(void)
    int x; 
    int count = 0;
    double mult = 1.0;
+   double ratio = RATIO;
+
+   if(argc>1)
+      ratio = atof(argv[1]);
+   printf("[%s], %f\n", argv[1], ratio);
 
    OpenAudioStream(&outStream, SAMPLE_RATE, paFloat32, PABLIO_WRITE|PABLIO_MONO);
 
@@ -31,7 +37,7 @@ int main(void)
            buffer[x] = mult * sin(theta); // ugly, I know...
            theta+= delta;
 	   if(!(count++ % incFrames))
-	      mult*= RATIO;
+	      mult*= ratio;
       }
 
       // blocking write provides flow control
