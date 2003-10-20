266db1cc4c94bdfd16a42afb806c465e163d85bf
keng <keng>
2003-10-20 21:12:21 +0000

    there was a problem in the random number generator (assumed a 16bit short instead of 32bit value)

--- Content/04Synthesis/2Cook/mandolin.c
+++ Content/04Synthesis/2Cook/mandolin.c
@@ -5,6 +5,7 @@
 /****************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "pablio.h"
 
 #define SAMPLE_RATE 22050.0
@@ -12,7 +13,7 @@
 #define NUM_FRAMES 1000
 
 int main(void) {
-   int i, j, k, dlength, dlength2, bufferIndex=0;
+   int i, j, k, dlength, dlength2;
    float scale[8] = {391.99,440.00,493.88,523.25, //  G scale frequencies
 		     587.32,659.25,739.99,783.99}; 
    short wg1[DMAX] = {0};                      // Waveguide delay line
@@ -24,54 +25,54 @@ int main(void) {
 	    0.772857, -0.671634, 0.648989, -0.690535,
 	    0.866150, -0.906709, 0.664901, -0.258501};
    float fnoise;
-   short output,output2;
+   short output, output2;
    short temp,slope;
-   short buffer[NUM_FRAMES];
    PABLIO_Stream *outStream;
 
    OpenAudioStream(&outStream, SAMPLE_RATE, paInt16, PABLIO_WRITE|PABLIO_MONO);
 
-   for (j=0;j<8;j++)   {                       // Play up the scale
-      dlength = 2 * SAMPLE_RATE / scale[j];             // Setup waveguide length
+   for (j=0;j<8;j++)   {                      // Play up the scale
+      dlength = 2 * SAMPLE_RATE / scale[j];   // Setup waveguide length
       dlength2 = dlength + 1;                 // Detune 2nd string 
       slope = 20000 / dlength;
       temp =  -10000;
+
       for (k=0;k<=dlength;k++) {              // Pluck
-	 fnoise = (float) rand();            // Noise for flavor
-	 fnoise = (fnoise - 16384.0) / 16384.0;
+	 fnoise = (float) rand();             // Noise for flavor
+	 fnoise = (fnoise - 1073741824.0) / 1073741824.0;
 	 fnoise = 1.0 + fnoise;
 	 wg1[k] = (temp * fnoise);
 	 wg2[k] = (temp * fnoise);
 	 temp += slope;
       }
-      for (i=0;i<SAMPLE_RATE/2;i++)   {               // Play each note for 1/2 second
-	 output = wg1[0];
+
+      for (i=0;i<SAMPLE_RATE/2;i++)   {      // Play each note for 1/2 second
+	 output  = wg1[0];
 	 output2 = wg2[0];
+
 	 for (k=0;k<dlength-1;k++) {         // Do delay lines
 	    wg1[k] = wg1[k+1];
 	    wg2[k] = wg2[k+1];
 	 }
+
 	 wg2[k] = wg2[k+1];                  // one extra delay
-	 wg1[dlength-1] = (output+oneZero)*0.4995;   // Do reflections
+	 wg1[dlength-1]  = (output+oneZero)  *0.4995;  // Do reflections
 	 wg2[dlength2-1] = (output2+oneZero2)*0.4995;  // Do reflections
-	 oneZero = output;
+	 oneZero  = output;
 	 oneZero2 = output2;
-	 output = (output + output2) * 0.05;
+	 output   = (output + output2) * 0.05;
+
 	 for (k=11;k>0;k--)  {
 	    output += 
 	       mandoBodyDelays[k] * mandoBodyCoeffs[k];
 	       mandoBodyDelays[k] = mandoBodyDelays[k-1];
 	 }
+
 	 output += mandoBodyDelays[0] * mandoBodyCoeffs[0];
 	 mandoBodyDelays[0] = output;
-	 buffer[bufferIndex++] = output;
-	 if (bufferIndex >= NUM_FRAMES){
-	    WriteAudioStream(outStream, buffer, bufferIndex+1);
-	    bufferIndex=0;
-	 }
+
+	 WriteAudioStream(outStream, &output, 1);
       }
    }
-   if (bufferIndex > 0)
-      WriteAudioStream(outStream, buffer, bufferIndex+1);
    CloseAudioStream(outStream);
 }
