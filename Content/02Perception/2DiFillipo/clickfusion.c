43608aee90ce33becce202e6deda0ed864bd8cc5
hgood <hgood>
2003-11-12 12:26:35 +0000

    add time delta parameter

--- Content/02Perception/2DiFillipo/clickfusion.c
+++ Content/02Perception/2DiFillipo/clickfusion.c
@@ -9,28 +9,44 @@
 
 float buffer[SAMPLE_RATE];
 
-int main(void)
+int main(int argc, char** argv)
 {
     PABLIO_Stream *outStream;
-    int sampleRate = SAMPLE_RATE;
+    float delta = 1.0;
+    int sampleLength;
     int i;
 
-    for (i = 0; i < SAMPLE_RATE; i++)
+    if (argc > 1)
+        delta = atof(argv[1]);
+    if (delta > 1.0) delta = 1.0;
+    if (delta < 0.0) delta = 0.0;
+
+    sampleLength = delta * SAMPLE_RATE;
+
+    for (i = 0; i < sampleLength; i++)
         buffer[i] = 1.0;
 
     OpenAudioStream(&outStream, SAMPLE_RATE, paFloat32,
         PABLIO_WRITE|PABLIO_MONO);
 
     // synthesize and output samples
-    while (sampleRate > 0) {
-        printf("sample rate %5d (%f seconds)\n",
-            sampleRate, (float) sampleRate/SAMPLE_RATE);
+    if (delta == 1.0) {
+        // default: loop delta from 1 second down to 0
+        while (sampleLength > 0) {
+            printf("sample rate %5d (%f seconds)\n", sampleLength, delta);
 
-        WriteAudioStream(outStream, &buffer, sampleRate);
+            WriteAudioStream(outStream, &buffer, sampleLength);
 
-        sampleRate *= 0.5;
+            delta *= 0.5;
+            sampleLength = delta * SAMPLE_RATE;
 
-        sleep(2);
+            sleep(2);
+        }
+    }
+    else {
+        printf("sample rate %5d (%f seconds)\n", sampleLength, delta);
+
+        WriteAudioStream(outStream, &buffer, sampleLength);
     }
 
     CloseAudioStream(outStream);
