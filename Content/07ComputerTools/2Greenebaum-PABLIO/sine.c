b273f3d1d491cccd91deb63658ea28c0f03e4181
hgood <hgood>
2003-11-12 12:30:50 +0000

    add frequency, attenuation parameters

--- Content/07ComputerTools/2Greenebaum-PABLIO/sine.c
+++ Content/07ComputerTools/2Greenebaum-PABLIO/sine.c
@@ -7,21 +7,29 @@
 
 float buffer[NUM_FRAMES];
 
-int main(void)
+int main(int argc, char *argv[])
 {
    PABLIO_Stream  *outStream;
-   double theta = 0.0, delta = FREQUENCY * 2.0 * 3.1415 / (double)SAMPLE_RATE;
+   double theta = 0.0, delta = 2.0 * 3.1415 / (double)SAMPLE_RATE;
+   double frequency = FREQUENCY, attenuation = 1.0;
    int x; 
+
+   if (argc > 1)
+      frequency = atof(argv[1]);
+   if (argc > 2)
+      attenuation = atof(argv[2]);
 
    OpenAudioStream(&outStream, SAMPLE_RATE, paFloat32, PABLIO_WRITE|PABLIO_MONO);
 
    while(1) {  // synthesize and output samples forever
       for(x= 0; x<NUM_FRAMES; x++) { // synthesize a buffer full of samples
-           buffer[x] = sin(theta); // ugly, I know...
-           theta+= delta;
+         buffer[x] = attenuation * sin(theta); // ugly, I know...
+         theta += frequency * delta;
       }
 
       // blocking write provides flow control
       WriteAudioStream(outStream, buffer, NUM_FRAMES); 
    }
+
+   CloseAudioStream(outStream);
 }
