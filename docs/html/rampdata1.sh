00d4dc680676c4473c5f7defec0b6fedc9135a0f
keng <keng>
2003-11-18 02:44:25 +0000

    fixed pathes

--- docs/html/rampdata1.sh
+++ docs/html/rampdata1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-Content/06ComputerTechniques/2Greenebaum-Test/ramptest < Content/06ComputerTechniques/2Greenebaum-Test/rampdata1
+../../Content/06ComputerTechniques/2Greenebaum-Test/ramptest < ../../Content/06ComputerTechniques/2Greenebaum-Test/rampdata1
 echo -n "Press enter to continue..."
 read n
--- docs/html/rampout.sh
+++ docs/html/rampout.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-Content/06ComputerTechniques/2Greenebaum-Test/rampout | Content/06ComputerTechniques/2Greenebaum-Test/ramptest
+../../Content/06ComputerTechniques/2Greenebaum-Test/rampout | ../../Content/06ComputerTechniques/2Greenebaum-Test/ramptest
 echo -n "Press enter to continue..."
 read n
