f79df41a78a5d9d2cb0f5df5da5d0a99915a6a32
keng <keng>
2004-05-19 06:34:42 +0000

    added missing code

--- Content2/03SignalProcessing/1Allman-FIR/fir.c
+++ Content2/03SignalProcessing/1Allman-FIR/fir.c
@@ -4,45 +4,48 @@
 //
 *********************************************************/
 
-#include <stdio.h>		/* printf debug info */           
+#include <stdio.h>		/* printf debug info         */
 #include <stdlib.h>		/* EXIT_SUCCESS return value */
 
-/* function prototype */
-
-float FIR_filter(float*,float*,int);
+float FIR_filter(float*, float*, int);
 
 int main()
 {
+   float seq[3]	    = {0.5, 1.0,-2.0};
+   float samples[3] = {0.0, 0.0, 0.0};
+   float coeff[3]   = {0.5, 1.0, 0.5};
+   float output	    = 0.0;
+   int no_coeff	    = 3;
 
-float seq[3]		= {0.5, 1.0,-2.0};
-float samples[3]	= {0.0, 0.0, 0.0};
-float coeff[3]		= {0.5, 1.0, 0.5};
-float output		= 0.0;
-int no_coeff		= 3;
 
 /* calls to FIR filter NOTE: 2n-1 calls to complete filtering
    normally this function would be called in a processing
    loop
 */      
 
-	samples[0] = seq[0];					/* load new sample */
-	output = FIR_filter(samples, coeff, no_coeff);  	/* perform filter  */    
-	printf("output 0:\t%1.2f\n", output);			/* print result    */
-	samples[0] = seq[1];
-	output = FIR_filter(samples, coeff, no_coeff);    
-    	printf("output 1:\t%1.2f\n", output);
-	samples[0] = seq[2];
-	output = FIR_filter(samples, coeff, no_coeff);    
-    	printf("output 2:\t%1.2f\n", output);
-	samples[0] = 0.0;
-	output = FIR_filter(samples, coeff, no_coeff);    
-    	printf("output 3:\t%1.2f\n", output);
-	samples[0] = 0.0;
-	output = FIR_filter(samples, coeff, no_coeff);    
-    	printf("output 4:\t%1.2f\n", output);
-    
-	return EXIT_SUCCESS;
+   samples[0] = seq[0];				   /* load new sample */
+   output = FIR_filter(samples, coeff, no_coeff);  /* perform filter  */    
+   printf("output 0:\t%1.2f\n", output);	   /* print result    */
+
+   samples[0] = seq[1];
+   output = FIR_filter(samples, coeff, no_coeff);    
+   printf("output 1:\t%1.2f\n", output);
+
+   samples[0] = seq[2];
+   output = FIR_filter(samples, coeff, no_coeff);    
+   printf("output 2:\t%1.2f\n", output);
+
+   samples[0] = 0.0;
+   output = FIR_filter(samples, coeff, no_coeff);    
+   printf("output 3:\t%1.2f\n", output);
+
+   samples[0] = 0.0;
+   output = FIR_filter(samples, coeff, no_coeff);    
+   printf("output 4:\t%1.2f\n", output);
+       
+   return EXIT_SUCCESS;
 }
+
 
 /**********************************************************
 //
@@ -64,15 +67,21 @@ int no_coeff		= 3;
 
 float FIR_filter(float* samples, float *coeff, int no_coeff)
 {
-static float output;	/* output of the FIR filter	*/
-static int i;		/* loop counter		*/
+   static float output;	/* output of the FIR filter  */
+   static   int      i;	/* loop counter		     */
 
-output = 0.0;		/* clear summing variable */
+   output = 0.0;	/* clear summing variable    */
 
-/* DEBUG: comment this part out if you do not wish to see 
-   the filtered data each time the function is called 
-*/   
-for(i = 0; i < no_coeff; i++)
-	printf("sample: %1.2f\tcoeff: %1.2f\n",samples[i],coeff[i]);
-/* END DEBUG */
+#ifdef DEBUG
+   for(i = 0; i < no_coeff; i++)
+      printf("sample: %1.2f\tcoeff: %1.2f\n", samples[i], coeff[i]);
+#endif /* DEBUG */
+
+   for (i = no_coeff-1; i >= 0; i--) {
+      output += samples[i] * coeff[i];
+      if (i < no_coeff-1)
+	 samples[i+1] = samples[i];
+   }
+
+   return output;
 }
