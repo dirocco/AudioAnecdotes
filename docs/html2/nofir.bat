1eb9cec9ea8313d8cd44dd02a95a3e6539b57d6d
howardg <howardg>
2004-06-14 20:14:16 +0000

    use thought.raw from Content2/media not src/streamutils

--- docs/html2/nofir.bat
+++ docs/html2/nofir.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Running rawfile2stream...
-..\..\bin\rawfile2stream ..\..\src\streamutils\thought.raw > thought.str
+..\..\bin\rawfile2stream ..\..\Content2\media\thought.raw > thought.str
 echo Running playstream.raw...
 ..\..\bin\playstream 1 44100 < thought.str
 exit
