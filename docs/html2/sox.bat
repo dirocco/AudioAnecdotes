bf2a08ba5b29b25029932e758f0bc6e0ece3ba37
howardg <howardg>
2004-07-01 06:28:09 +0000

    fix some bugs James reported

--- docs/html2/sox.bat
+++ docs/html2/sox.bat
@@ -2,4 +2,5 @@
 echo Running sox...
 if %1==n ..\..\bin\sox -r 44100 -c 1 -w -s ..\..\Content2\media\thought.raw thought.wav
 if %1==r ..\..\bin\sox -r 44100 -c 1 -w -s ..\..\Content2\media\thought.raw thought.wav reverb 1 800 400 200
+chmod -x thought.wav
 exit
