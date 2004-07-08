92baf224b7dabd29c48b9b897bb5bc2e42586c78
keng <keng>
2004-07-08 08:59:39 +0000

    initial

---
+++ docs/html2/ifir.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running fir2 interactively
+..\..\Content2\03SignalProcessing\1Allman-FIR\fir2 ..\..\Content2\03SignalProcessing\1Allman-FIR\coefs.txt
+exit
---
+++ docs/html2/iiir.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running iir2 interactively
+..\..\Content2\03SignalProcessing\2Allman-IIR\iir2 ..\..\Content2\03SignalProcessing\2Allman-IIR\coefs.txt
+exit
