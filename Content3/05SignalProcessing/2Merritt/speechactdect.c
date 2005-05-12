57e7894dc89c5a1ae05e993fc6ac75363db51a60
howardg <howardg>
2005-05-12 23:06:55 +0000

    use packet size from Ian's paper

--- Content3/05SignalProcessing/2Merritt/speechactdect.c
+++ Content3/05SignalProcessing/2Merritt/speechactdect.c
@@ -1,13 +1,43 @@
-#include <math.h>     // sin()
 #include "pablio.h"
 
 #define SAMPLE_RATE  8000
-#define FREQUENCY     440.0
-#define NUM_FRAMES     10
+#define NUM_FRAMES    180
 #define FILENAME "voice1-mu.raw"
-// #define FILENAME "voice1.raw"
 
-int main(void)
+/*
+** This routine converts from ulaw to 16 bit linear.
+**
+** Craig Reese: IDA/Supercomputing Research Center
+** 29 September 1989
+**
+** References:
+** 1) CCITT Recommendation G.711  (very difficult to follow)
+** 2) MIL-STD-188-113,"Interoperability and Performance Standards
+**     for Analog-to_Digital Conversion Techniques,"
+**     17 February 1987
+**
+** Input: 8 bit ulaw sample
+** Output: signed 16 bit linear sample
+*/
+
+int
+ulaw2linear(ulawbyte)
+unsigned char ulawbyte;
+{
+  static int exp_lut[8] = {0,132,396,924,1980,4092,8316,16764};
+  int sign, exponent, mantissa, sample;
+
+  ulawbyte = ~ulawbyte;
+  sign = (ulawbyte & 0x80);
+  exponent = (ulawbyte >> 4) & 0x07;
+  mantissa = ulawbyte & 0x0F;
+  sample = exp_lut[exponent] + (mantissa << (exponent + 3));
+  if (sign != 0) sample = -sample;
+
+  return(sample);
+}
+
+int main(int argc, char **argv)
 {
    FILE *input;
    PABLIO_Stream  *outStream;
@@ -16,6 +46,7 @@ int main(void)
    int count=16;
    double minavg=0.0,avg=0.0,thresh;
    char outbuffer[NUM_FRAMES];
+   char zerobuffer[NUM_FRAMES] = { 0 };
    int got= 1;
    int x;
 
@@ -24,42 +55,52 @@ int main(void)
    }else{
      OpenAudioStream(&outStream,SAMPLE_RATE,paInt8,PABLIO_WRITE|PABLIO_MONO);
 
-      while(got){
+      x = 0;
+      while(1){
          got = fread(&charSample, 1, 1, input);
+         if (got)
          {
-       //     printf("%08d %02X\n", x++,(char)charSample);
-	    outbuffer[0]= (short)charSample;
-
-//printf("%d",outbuffer[0]); 
-//	    WriteAudioStream(outStream, outbuffer, 1);
-
             sample = charSample;
 
 	    // process every sample
             avg = (fabs(sample)/256.0) + ((255.0 * avg)/256.0);
-//printf("sample %f, avg %f\n",sample, avg);
- 
-// XXX FIX THIS !!!!  See next line...
+
+            outbuffer[x++] = ulaw2linear(charSample) / 256.0;
+
+            if (x < NUM_FRAMES)
+               continue;
+
 	    // this computation is supposed to run once per 22.5ms parcel...
             thresh = minavg + 8;
             if (avg > thresh){
-//printf("here %f\n",avg);
-		outbuffer[0]=(short)sample;
+printf("buff %f %f %f %d\n",avg,minavg,thresh,count);
+                WriteAudioStream(outStream,outbuffer,NUM_FRAMES);
 		count -=1;
 		if (count == 0){
 			minavg += 1;
 			count = 16;
 		}
 	    }else{
-// printf("avg %X\n",avg);
-		outbuffer[0] = 0;
+printf("zero %f %f %f %d\n",avg,minavg,thresh,count);
+                WriteAudioStream(outStream,zerobuffer,NUM_FRAMES);
 		if (avg < minavg){
 			minavg = avg;
 			count = 16;
                 }
              }
-             WriteAudioStream(outStream,outbuffer,1);
+            x = 0;
         }
+        else
+           rewind(input);
      }
+
+     if (avg > thresh)
+        WriteAudioStream(outStream,outbuffer,x);
+     else
+        WriteAudioStream(outStream,zerobuffer,x);
+
+     CloseAudioStream(outStream);
+
+     fclose(input);
   }   
 }
